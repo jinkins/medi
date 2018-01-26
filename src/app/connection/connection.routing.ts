583389aa6664067ae97608848d3fa5a8a8c96bb6
jinkins <nick-1138@hotmail.com>
2018-01-26 09:16:18 +0100

    EDIT Form

--- src/app/connection/connection.routing.ts
+++ src/app/connection/connection.routing.ts
@@ -2,9 +2,11 @@ import { SearchConnectionsComponent } from './search-connections/search-connecti
 import { AuthGuard } from './../shared/auth-guard.service';
 import { Routes } from '@angular/router';
 import { ShowAllConnectionsComponent } from './show-all-connections/show-all-connections.component';
+import { EditConnectionComponent } from './edit-connection/edit-connection.component';
 
 export const connectionsRouting: Routes = [
     { path: 'showAll', component: ShowAllConnectionsComponent, canActivate: [AuthGuard] },
     { path: 'find', component: SearchConnectionsComponent, canActivate: [AuthGuard] },
+    { path: 'new', component: EditConnectionComponent, canActivate: [AuthGuard] },
     { path: '', redirectTo: 'showAll',  pathMatch: 'full'  }
 ];
--- src/app/connection/edit-connection/edit-connection.component.ts
+++ src/app/connection/edit-connection/edit-connection.component.ts
@@ -1,4 +1,8 @@
 import { Component, OnInit } from '@angular/core';
+import { TPService } from '../tp/tp.service';
+import { TP } from '../tp/tp';
+import { Router } from '@angular/router';
+import { FormBuilder, FormGroup } from '@angular/forms';
 
 @Component({
   selector: 'app-edit-connection',
@@ -7,9 +11,64 @@ import { Component, OnInit } from '@angular/core';
 })
 export class EditConnectionComponent implements OnInit {
 
-  constructor() { }
+  constructor(private ts: TPService, private router: Router, private fb: FormBuilder) { }
+
+  public tps: TP[];
+  public selectedTP: string;
+  public connectionForm: FormGroup;
+  public editMode = false;
 
   ngOnInit() {
+    if (this.editMode == true) {
+
+    } else {
+      this.connectionForm = this.fb.group({
+        gln: '',
+        message: '',
+        actif: '0',
+        copy: '',
+        as2id: '',
+        as2url: '',
+        tpName: 'pending',
+        c: '',
+        admd: '',
+        prmd: '',
+        o: '',
+        ou1: '',
+        ou2: '',
+        ou3: '',
+        g: '',
+        i: '',
+        s: ''
+      });
+    }
+
+
+
+    this.ts.getTPs().subscribe(
+      // retrieve all TPS
+      (data) => {
+        this.tps = data;
+
+
+
+      },
+      error => {
+        console.log(error);
+        if (error.status === 401) {
+          this.router.navigateByUrl('/login');
+        }
+      });
+
+
+  }
+
+  displayAS2() {
+    if (this.connectionForm.value['tpName'] == 'ATLAS') {
+      return false;
+    } else {
+      return true;
+    }
   }
 
 }
